2a1801707594603116f8822ca8816bdefd18114f
tarekfb <56593098+tarekfb@users.noreply.github.com>
2020-06-11 15:31:58 +0200

    Update i2.js

--- Malmö högskola/Webutveckling 2 - Klient/i2.js
+++ Malmö högskola/Webutveckling 2 - Klient/i2.js
@@ -30,13 +30,39 @@ function min(firstVal, SecondVal){
  */
 
 function range(n){
+	let numbersArray = [];
+	numbersArray.length = n;
+
+
+	for (var i = 0; i < numbersArray.length; i++) {
+		numbersArray[i] = i;
+	}
 	
+	return numbersArray;
 }
+
+var testArray = range(10);
+console.log(testArray);
+
 
 /**
  * Uppgift 3
  * =========
  */
+
+function sum(array) {
+	let sum = 0;
+	let numbers = array;
+
+	for (var i = 0; i < array.length; i++) {
+		sum += array[i];
+	}
+	return sum;
+}
+
+var numbers = [5, 10, 15, 20, 25];
+var sumOfNumbers = sum(numbers);
+console.log(sumOfNumbers);
 
 
 /**
@@ -44,6 +70,19 @@ function range(n){
  * =========
  */
 
+function countCharacter(string, char) {
+	let nbrOfTimes = 0;
+	for (var i = 0; i < string.length; i++) {
+		if (string[i] == char){
+			nbrOfTimes += 1;
+		}
+	}
+	console.log(nbrOfTimes);
+	return nbrOfTimes;
+}
+
+countCharacter("aasd", 'a');
+countCharacter("aasd", 'b');
 
 
 /**
@@ -51,7 +90,20 @@ function range(n){
  * =========
  */
 
+function palindrome(string) {
+	let palindromeApproved;
+	let reversedString = string.split("").reverse().join("");
 
+	if (string == reversedString){
+		palindromeApproved = true;
+	} else if (string != reversedString){
+		palindromeApproved = false;
+	}
+	return palindromeApproved;
+}
+
+console.log(palindrome("asd"));
+console.log(palindrome("sirap i paris"));
 
 /**
  * Uppgift 7
