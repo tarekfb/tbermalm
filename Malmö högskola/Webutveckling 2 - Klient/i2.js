69f42168eda7133b18ffec1557bde58f79a60447
tarekfb <56593098+tarekfb@users.noreply.github.com>
2020-06-11 14:23:11 +0200

    i2

---
+++ Malmö högskola/Webutveckling 2 - Klient/i2.js
@@ -0,0 +1,79 @@
+"use strict";
+/* Tarek Bermalm - AK6336. */
+
+/**
+ * Uppgift 1
+ * =========
+ */
+
+function max(firstVal, SecondVal){
+	if (firstVal > SecondVal){
+		return firstVal;
+	}
+	else if (SecondVal > firstVal){
+		return SecondVal;
+	}
+}
+
+function min(firstVal, SecondVal){
+	if (firstVal < SecondVal){
+		return firstVal;
+	}
+	else if (SecondVal < firstVal){
+		return SecondVal;
+	}
+}
+
+/**
+ * Uppgift 2
+ * =========
+ */
+
+function range(n){
+	
+}
+
+/**
+ * Uppgift 3
+ * =========
+ */
+
+
+/**
+ * Uppgift 4
+ * =========
+ */
+
+
+
+/**
+ * Uppgift 5
+ * =========
+ */
+
+
+
+/**
+ * Uppgift 7
+ * =========
+ */
+
+
+
+ /**
+ * Uppgift 8
+ * =========
+ */
+
+ /**
+ * Uppgift 9
+ * =========
+ */
+
+
+
+ /**
+ * Uppgift 10
+ * =========
+ */
+
