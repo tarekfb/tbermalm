845d3cc4fec7e7ed81a73f51637af23d454db749
tarekfb <56593098+tarekfb@users.noreply.github.com>
2020-06-14 14:53:08 +0200

    i3

--- Malmö högskola/Webutveckling 2 - Klient/i3.js
+++ Malmö högskola/Webutveckling 2 - Klient/i3.js
@@ -14,14 +14,24 @@ function changeClass(id){
  * =========
  */
 
-
+function addItem() {
+	var liValue = prompt("Please enter some text");
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(liValue));
+	var ul = document.getElementById("items");
+	ul.appendChild(li);
+}
 
 /**
  * Uppgift 3
  * =========
  */
 
+Uppgift 3
 
+I den tredje uppgiften ska ni komplettera Uppgift 2 med en extra knapp. Ni väljer själv id och text. Denna knapp ska, när en användare klickar på den, radera det sista elementet i listan <ul id="items">.
+
+Tips: använd er av .lastElementChild i kombination med .removeChild.
 
 /**
  * Uppgift 4
