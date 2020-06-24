4e94398dda55c5a82537e48b97eac7c989652a42
tarekfb <56593098+tarekfb@users.noreply.github.com>
2020-06-24 11:57:06 +0200

    Update i4.js

--- Malmö högskola/Webutveckling2_Klient/i4.js
+++ Malmö högskola/Webutveckling2_Klient/i4.js
@@ -10,6 +10,8 @@ form.addEventListener("submit", function(event) {
     	addListItem("Please enter something.");
     	document.getElementById("items").style.listStyle = "none";
 	} else {
+		document.getElementById("items").style.listStyle = "default";
+
 		let queryText = form.elements.query.value;
 		apiHandler(encodeURI(queryText));
 	}
