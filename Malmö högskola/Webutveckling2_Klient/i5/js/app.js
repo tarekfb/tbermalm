70d3439c3d8f71d40f885b7ea3d9c7e165e1f51e
tarekfb <56593098+tarekfb@users.noreply.github.com>
2020-07-01 18:15:26 +0200

    react

---
+++ Malmö högskola/Webutveckling2_Klient/i5/js/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'i5/css/styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+          Learn React
+      </header>
+    </div>
+  );
+}
+export default App;
