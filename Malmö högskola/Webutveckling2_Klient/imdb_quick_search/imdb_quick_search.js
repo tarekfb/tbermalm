d04eb25779f9aca4309addb15da6c67166e5c504
tarekfb <56593098+tarekfb@users.noreply.github.com>
2020-07-16 13:25:37 +0200

    Testing if passing imdbSearchEntry works

--- Malmö högskola/Webutveckling2_Klient/imdb_quick_search/imdb_quick_search.js
+++ Malmö högskola/Webutveckling2_Klient/imdb_quick_search/imdb_quick_search.js
@@ -33,7 +33,8 @@ function apiHandler(title) {
 	    	//also wrap it in a link to IMDB page
 	    	result.Search.forEach(function(entry) {
   				addListItem(
-  					entry.Title + " (" + String(entry.Year) + ")" + ", " + parseInt(entry.imdbRating) + "\n", entry.imdbID
+  					addListItem(entry);
+  					//entry.Title + " (" + String(entry.Year) + ")" + ", " + parseInt(entry.imdbRating) + "\n", entry.imdbID
   				);
   				//trying to add imdbrating but failing for some reason
 			});
@@ -47,9 +48,9 @@ function apiHandler(title) {
 	omdbAPI.send();
 }
 
-function addListItem(string, imdbID) {
+function addListItem(imdbSearchEntry) {
 	//just generating nodes as neccessary, to display results
-	if (imdbID == null){
+	/*if (imdbID == null){
 		if (string == "Too many results."){
 			string += " Try to be more specific."
 		} else if (string == "Movie not found!"){
@@ -78,7 +79,8 @@ function addListItem(string, imdbID) {
 
 		let containerDiv = document.getElementById("result");
 		containerDiv.appendChild(ul);
-	}
+	}*/
+	console.log(imdbSearchEntry);
 
 }
 
