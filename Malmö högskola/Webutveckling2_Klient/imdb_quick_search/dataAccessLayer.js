fc7958c1efe616239bcbb6634b0822f94fef3d13
tarekfb <56593098+tarekfb@users.noreply.github.com>
2020-10-18 19:03:49 +0200

    working on firebase

--- Malmö högskola/Webutveckling2_Klient/imdb_quick_search/dataAccessLayer.js
+++ Malmö högskola/Webutveckling2_Klient/imdb_quick_search/dataAccessLayer.js
@@ -1,13 +1,69 @@
-window.onload = function (){
-    document.addEventListener("DOMContentLoaded", event => {
-        const app = firebase.app();
-        const db = firebase.database();
-        const myPost = db.collection('posts').doc('firstpost');
+initializeFireBase();
 
-        myPost.get().
-        then(doc => {
-            const data = doc.data();
-            console.log(data.title)
-        })
+let db = firebase.database();
+let ref = db.ref("simple-imdb-search");
+ref.on("value", gotData, errData);
+
+function writeToDb(imdbID) {
+    db.ref('movie-list/').set({
+        imdbID: imdbID
     });
+}
+writeToDb("notrealimdbid");
+
+
+function gotData(data) {
+
+}
+function errData(err) {
+    console.log('errror' + err);
+}
+
+function initializeFireBase() {
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    let firebaseConfig = {
+        apiKey: "AIzaSyDNoxennmAjR3Yjh9Rpe4ahpHv0zYcdO9Y",
+        authDomain: "simple-imdb-search.firebaseapp.com",
+        databaseURL: "https://simple-imdb-search.firebaseio.com",
+        projectId: "simple-imdb-search",
+        storageBucket: "simple-imdb-search.appspot.com",
+        messagingSenderId: "875771108342",
+        appId: "1:875771108342:web:63ed0e9505a5ac087b45d7",
+        measurementId: "G-NZGQ93J58S"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+}
+
+function handleSignIn() {
+    //anon sign in
+    //https://firebase.google.com/docs/auth/web/anonymous-auth?authuser=0
+
+    //get started with firebase security
+    //https://firebase.google.com/docs/database/security/get-started?authuser=0
+
+  /*
+    firebase.auth().signInAnonymously().catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+    });
+
+   firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+  }
+  // ...
+});
+
+   */
 }
