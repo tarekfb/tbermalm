98e481059674d0523a79e24fab11dcd46b157c02
tarekfb <56593098+tarekfb@users.noreply.github.com>
2021-02-17 15:25:24 +0100

    Testing serverless-http with netfliy deployment
    Netlify runs on Jamstack architecture, which means Nodejs express functionality is severely restricted. Per netlify docs, there is some functionality to server files and use REST api through a serverless environment. Testing ensues...

--- car-shop/node_scripts/api.js
+++ car-shop/node_scripts/api.js
@@ -1,23 +1,50 @@
-//"use strict";
+// Version 1.0
+// const express = require('express');
+// const path = require("path");
+// const indexRouter = require('./routes/index');
+// const carmodelsRouter = require('./routes/carmodels');
+// const employeesRouter = require('./routes/employees');
+//
+// const app = express();
+//
+// // Handle static files in public dir
+// app.use(express.static(path.join(__dirname, "..", 'public')));
+//
+// // Use respective router js file to handle all the specific endpoints
+// app.use('/', indexRouter);
+// app.use("/carmodels", carmodelsRouter);
+// app.use("/employees", employeesRouter);
+//
+// // Setting up server
+// const port = process.env.PORT || 4000
+// let server = app.listen(port, () => {
+//   let port = server.address().port;
+//   console.log("Listening on port", port);
+// });
+
+// Version 2.0: Serverless, netflify
+
 const express = require('express');
 const path = require("path");
 const indexRouter = require('./routes/index');
 const carmodelsRouter = require('./routes/carmodels');
 const employeesRouter = require('./routes/employees');
 
+const serverless = require('serverless-http');
 const app = express();
 
-// Handle static files in public dir
-app.use(express.static(path.join(__dirname, "..", 'public')));
-
-// Use carmodels.js file to handle all "carmodels" endpoints
-app.use('/', indexRouter);
-app.use("/carmodels", carmodelsRouter);
-app.use("/employees", employeesRouter);
-
-// Setting up server
-const port = process.env.PORT || 4000
-let server = app.listen(port, () => {
-  let port = server.address().port;
-  console.log("Listening on port", port);
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
 });
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(express.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
