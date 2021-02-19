c4c1272419442a4c63770d54bfd1138c4afcd3cd
tarekfb <56593098+tarekfb@users.noreply.github.com>
2021-02-19 20:12:04 +0100

    stable functionality for handling users
    - log in
    - log out
    - sign up
    - reset password

--- car-shop/node_scripts/routes/profile.js
+++ car-shop/node_scripts/routes/profile.js
@@ -4,29 +4,41 @@ const firebase = require(path.join("./..", "firebase"));
 const router = express.Router();
 router.use(express.json());
 
+//TODO: make global scope variables for auth() and rootRef
+
+// Middleware to check if user is logged out, and allow if so
 // TODO: change url on both of these middlewares to regex or condition: |
 router.use("/login", (req, res, next) => {
   if (firebase.auth().currentUser){
     console.log("already logged in");
-    res.sendStatus(403)
+    res.sendStatus(400)
   } else {
-    console.log("else was called");
     next();
   }
 });
 router.use("/signup", (req, res, next) => {
   if (firebase.auth().currentUser){
     console.log("already logged in");
-    res.sendStatus(403)
+    res.sendStatus(400)
   } else {
-    console.log("else was called");
     next();
   }
 });
 
+// Middleware to check if user is logged in, and allow if so
+router.use("/reset-password", (req, res, next) => {
+  if (firebase.auth().currentUser){
+    next();
+  } else {
+    res.sendStatus(400)
+  }
+});
+
+
 router.get("/", (req, res) => {
   if (firebase.auth().currentUser){
-    res.send({"result": true});
+    let user = firebase.auth().currentUser;
+    res.send({"result": true, "user": user});
   } else {
     res.send({"result": false});
   }
@@ -40,12 +52,14 @@ router
       .then((userCredential) => {
         // Signed in
         let user = userCredential.user;
-        res.send(200);
+        res.status(200);
+        res.send(user);
+        //res.send(200);
       })
       .catch((error) => {
         let errorCode = error.code;
         let errorMessage = error.message;
-        res.sendStatus(401)
+        res.sendStatus(400)
       });
 
   });
@@ -56,17 +70,16 @@ router
     firebase.auth().signInWithEmailAndPassword(req.body.username, req.body.password)
       .then((userCredential) => {
         // Signed in
-        console.log("successful login");
         let user = userCredential.user;
-        res.sendStatus(200);
+        res.status(200);
+        res.send(user);
 
       })
       .catch((error) => {
-        console.log("error at login");
-
         let errorCode = error.code;
         let errorMessage = error.message;
-        res.sendStatus(401);
+        res.status(400);
+        res.send(error);
       });
   });
 
@@ -74,13 +87,23 @@ router
   .route("/logout")
   .post((req, res) => {
     firebase.auth().signOut().then(() => {
-      console.log("loggedout")
-
+      res.sendStatus(200);
     }).catch((error) => {
-      // An error happened.
-      console.log("failed logout");
+      res.status(400);
+      res.send(error);
     });
-    res.send();
   });
+
+router.get("/reset-password", (req, res) => {
+  let auth = firebase.auth();
+  let emailAddress = "user@example.com";
+
+  firebase.auth().sendPasswordResetEmail(firebase.auth().currentUser.email).then(function() {
+    res.sendStatus(200);
+  }).catch((error) => {
+    res.status(400);
+    res.send(error);
+  });
+});
 
 module.exports = router;
--- car-shop/public/javascripts/client-profile.js
+++ car-shop/public/javascripts/client-profile.js
@@ -4,7 +4,9 @@ $(document).ready(() => {
    * Init
    ****************************************/
 
+  isLoggedIn();
 
+  //TODO: make global scope var for http://" + window.location.host + "/profile/"
 
   /***************************************
    * CRUD frontend interactions
@@ -26,6 +28,9 @@ $(document).ready(() => {
   $("#logout-button").on("click", () => {
     initLogOut();
   });
+  $("#reset-password").on("click", () => {
+    initPasswordReset();
+  });
 
   /***************************************
    * HTTP requests and related functions
@@ -36,7 +41,7 @@ $(document).ready(() => {
       url: "http://" + window.location.host + "/profile/", // In prod env, change url
       type: 'GET',
       contentType:"application/json",
-      success: (response) => handleSignIn(response),
+      success: (response) => handleAuthState(response),
       error: function (xhr, status, error) {
         console.log(`Error signup: ${error}`);
         $('#response').html('Error');
@@ -49,7 +54,7 @@ $(document).ready(() => {
       type: 'POST',
       data: JSON.stringify(userCredentials),
       contentType:"application/json",
-      success: (response) => console.log(response),
+      success: (response) => onLoggedIn(response), //TODO: change to response.user
       error: function (xhr, status, error) {
         console.log(`Error signup: ${error}`);
         $('#response').html('Error');
@@ -62,7 +67,7 @@ $(document).ready(() => {
       type: 'POST',
       data: JSON.stringify(userCredentials),
       contentType:"application/json",
-      success: (response) => handleSignIn(response),
+      success: (response) => onLoggedIn(response),
       error: function (xhr, status, error) {
         console.log(`Error login: ${error}`);
         $('#response').html('Error');
@@ -74,7 +79,7 @@ $(document).ready(() => {
       type: 'POST',
       data: JSON.stringify(userCredentials),
       contentType: "application/json",
-      success: (response) => console.log(response),
+      success: (response) => onLoggedOut(response),
       error: function (xhr, status, error) {
         console.log(`Error login: ${error}`);
         $('#response').html('Error');
@@ -82,12 +87,37 @@ $(document).ready(() => {
     });
   }
 
-  function handleAuthState() {
+  function initPasswordReset() {
+    $.ajax({
+      url: "http://" + window.location.host + "/profile/reset-password", // In prod env, change url
+      type: 'GET',
+      success: (response) => console.log(response),
+      error: function (xhr, status, error) {
+      console.log(`Error signup: ${error}`);
+      $('#response').html('Error');
+    }});
 
   }
 
-  function handleSignIn() {
-    //$("#auth-form").toggleClass("d-none");
+  function handleAuthState(response) {
+    if (response.result){
+      onLoggedIn(response.user);
+    } else if (!response.result){
+      onLoggedOut();
+    }
   }
+
+  function onLoggedIn(user) {
+    $("#authed-container").removeClass("d-none");
+    $("#not-authed-container").addClass("d-none");
+
+    $("#email-field").find("span").text(user.email);
+  }
+
+  function onLoggedOut() {
+    $("#authed-container").addClass("d-none");
+    $("#not-authed-container").removeClass("d-none");
+  }
+
 
 });
